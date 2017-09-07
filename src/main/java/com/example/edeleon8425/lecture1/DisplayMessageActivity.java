4346496ea44151695966a3655e2125beda28143e
Emily De Leon <Edeleon8425@conestogac.on.ca>
2017-09-07 10:45:52 -0400

    Lecture 1 splashscreen

---
+++ src/main/java/com/example/edeleon8425/lecture1/DisplayMessageActivity.java
@@ -0,0 +1,13 @@
+package com.example.edeleon8425.lecture1;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
+/**
+ * Created by edeleon8425 on 9/7/2017.
+ */
+
+class DisplayMessageActivity {
+
+}
